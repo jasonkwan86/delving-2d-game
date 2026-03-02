020ef94e0c50a5d5607a47f841a178fc99c8feb2
Jason Kwan <kwan.jason2001@gmail.com>
2026-03-01 22:17:39 -0500

    straighter squarelike herringbone generation

--- common/cave_generation/herringbone_wang_world/herringbone_wang_generator.gd
+++ common/cave_generation/herringbone_wang_world/herringbone_wang_generator.gd
@@ -13,10 +13,15 @@ func _input(event):
 		get_tree().reload_current_scene()
 
 func generate_herringbone_map(width = 20, height = 20) -> void:
-	tilemap.clear()
 	for row in height:
-		for col in range(0, width, 4):
-			_place_dual_corner_tiles(row, col + row)
+		if row % 4 == 3:
+			# Last row, offset by -1 horizontally from the first row
+			for col in range(0, width, 4):
+				_place_dual_corner_tiles(row, col - 1)
+		else:
+			# First 3 sequential rows
+			for col in range(0, width, 4):
+				_place_dual_corner_tiles(row, col + row % 4)
 
 func _place_dual_corner_tiles(row: int, col: int) -> void:
 	herringbone_wang_data_set.copy_random_hbwt_horizontal_to_tile_map_layer(col * Vector2i.RIGHT + row * Vector2i.DOWN, tilemap)
