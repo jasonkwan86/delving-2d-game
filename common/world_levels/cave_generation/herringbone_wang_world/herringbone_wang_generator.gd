2875b5f80746804612dd89b3b43f9c43018fc147
Jason Kwan <kwan.jason2001@gmail.com>
2026-03-10 20:36:47 -0400

    add random generation below hub, starting level

--- common/world_levels/cave_generation/herringbone_wang_world/herringbone_wang_generator.gd
+++ common/world_levels/cave_generation/herringbone_wang_world/herringbone_wang_generator.gd
@@ -6,22 +6,22 @@ extends Node
 
 func _ready():
 	randomize()
-	generate_herringbone_map()
+	generate_herringbone_map(Vector2i(-5, 5), Vector2i(20, 20))
 
 #func _input(event):
 	#if event.is_action_pressed("ui_accept"):
 		#get_tree().reload_current_scene()
 
-func generate_herringbone_map(width = 20, height = 20) -> void:
-	for row in height:
+func generate_herringbone_map(offset: Vector2i = Vector2i(0, 0), dimensions: Vector2i = Vector2i(20, 20)) -> void:
+	for row in dimensions.y:
 		if row % 4 == 3:
 			# Last row, offset by -1 horizontally from the first row
-			for col in range(0, width, 4):
-				_place_dual_corner_tiles(row, col - 1)
+			for col in range(0, dimensions.x, 4):
+				_place_dual_corner_tiles(row + offset.y, col - 1 + offset.x)
 		else:
 			# First 3 sequential rows
-			for col in range(0, width, 4):
-				_place_dual_corner_tiles(row, col + row % 4)
+			for col in range(0, dimensions.x, 4):
+				_place_dual_corner_tiles(row + offset.y, col + row % 4 + offset.x)
 
 func _place_dual_corner_tiles(row: int, col: int) -> void:
 	herringbone_wang_data_set.copy_random_hbwt_horizontal_to_tile_map_layer(col * Vector2i.RIGHT + row * Vector2i.DOWN, tilemap)
