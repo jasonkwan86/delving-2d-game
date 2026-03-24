fcedd6157b9d168bdd69706b805e0aa7fd4fac4b
Jason Kwan <kwan.jason2001@gmail.com>
2026-03-24 19:47:53 -0400

    add mining

---
+++ common/player/mining_component/mining_component.gd
@@ -0,0 +1,35 @@
+class_name MiningComponent
+extends Node2D
+
+@export_category("World Properties")
+@export var player: CharacterBody2D
+@export var world_tilemap: TileMapLayer
+@export var break_tilemap: TileMapLayer
+@export var block_break_states: int = 3
+
+@export_category("Mining Properties")
+@export var mine_range: float = 20
+@export var mine_damage_per_second: float = 100
+
+var cell_healths: Dictionary[Vector2i, float]
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("mine"):
+		mine(delta)
+
+func mine(delta: float) -> void:
+	if player.global_position.distance_to(get_global_mouse_position()) > mine_range:
+		return
+	var cell_position: Vector2i = world_tilemap.local_to_map(world_tilemap.to_local(get_global_mouse_position()))
+	if world_tilemap.get_cell_source_id(cell_position) == -1:
+		return
+	
+	var max_health = world_tilemap.get_cell_tile_data(cell_position).get_custom_data("health")
+	if cell_position not in cell_healths:
+		cell_healths[cell_position] = max_health
+	cell_healths[cell_position] -= mine_damage_per_second * delta
+	
+	break_tilemap.set_cell(cell_position, 0, Vector2i(lerp(block_break_states + 1, 0, cell_healths[cell_position] / max_health), 0))
+	if cell_healths[cell_position] <= 0:
+		world_tilemap.erase_cell(cell_position)
+		break_tilemap.erase_cell(cell_position)
