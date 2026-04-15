921d3da34c976b30da5aa135478e5907d7ddde84
Jaree Khan <jaree@Jarees-MacBook-Air.local>
2026-04-15 19:29:53 -0400

    added player animations

--- common/player/player.gd
+++ common/player/player.gd
@@ -6,9 +6,13 @@ const JUMP_VELOCITY = -200.0
 @export var max_health: int = 5
 var health: int
 var invincible: bool = false
+ 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var hud: CanvasLayer
 var health_bar: ProgressBar
+
+var is_hurt: bool = false
 
 func _ready():
 	add_to_group("player")
@@ -45,11 +49,28 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	#flip character
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true 
+	
+	#play animations
+	if not is_hurt:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite_2d.play("idle")
+			else:
+				animated_sprite_2d.play("run") 
+		else:
+			animated_sprite_2d.play("fall")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 
 	# Stomp detection
@@ -72,19 +93,29 @@ func _physics_process(delta: float) -> void:
 func take_damage(amount: int):
 	if invincible:
 		return
+	
 	health -= amount
 	health_bar.value = health
 	if health <= 0:
 		die()
 		return
 	invincible = true
+	#play hurt animatoin
+	is_hurt = true
+	animated_sprite_2d.play("hurt")
+	await get_tree().create_timer(0.3).timeout
+	is_hurt = false
+	
 	await get_tree().create_timer(1.0).timeout
 	invincible = false
 
 func die():
 	set_physics_process(false)
 	set_process_input(false)
-
+	#play die animation
+	animated_sprite_2d.play("die")
+	await get_tree().create_timer(2.5).timeout
+	
 	var overlay = ColorRect.new()
 	overlay.color = Color(0, 0, 0, 0.6)
 	overlay.set_anchors_preset(Control.PRESET_FULL_RECT)
